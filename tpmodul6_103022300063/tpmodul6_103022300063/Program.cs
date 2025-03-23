c04327bba04f4f190052bac5dcfab0588a71fd55
adiityanugraha <150656990+adiityanugraha@users.noreply.github.com>
2025-03-23 12:55:59 +0700

    Add files via upload
    menambahkan class SayaTubeVideo

--- tpmodul6_103022300063/tpmodul6_103022300063/Program.cs
+++ tpmodul6_103022300063/tpmodul6_103022300063/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+public class Program {
+    public static void Main() {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Anak Agung Aryadipa Aditya Nugraha");
+
+        video.IncreasePlayCount(100);
+
+        video.PrintVideoDetails();
+    }
+}
+
---
+++ tpmodul6_103022300063/tpmodul6_103022300063/SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    // Konstruktor
+    public SayaTubeVideo(string videoTitle)
+    {
+        playCount = 0;
+
+        Random rand = new Random();
+        id = rand.Next(10000, 100000);
+
+        title = videoTitle;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count > 0)
+        {
+            playCount += count;
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID Video: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+    }
+}
